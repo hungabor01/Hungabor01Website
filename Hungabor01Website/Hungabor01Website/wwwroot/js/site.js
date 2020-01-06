d77202564c6be2b599076bdfed62ea5b98361044
hungabor01 <hungabor01@gmail.com>
2020-01-06 19:09:50 +0100

    Navigation

--- Hungabor01Website/Hungabor01Website/wwwroot/js/site.js
+++ Hungabor01Website/Hungabor01Website/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿jQuery(function ($) {
 
-// Write your JavaScript code.
+  $(".sidebar-dropdown > a").click(function () {
+    $(".sidebar-submenu").slideUp(200);
+    if (
+      $(this)
+        .parent()
+        .hasClass("active")
+    ) {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .parent()
+        .removeClass("active");
+    } else {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .next(".sidebar-submenu")
+        .slideDown(200);
+      $(this)
+        .parent()
+        .addClass("active");
+    }
+  });
+
+  $("#close-sidebar").click(function () {
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $("#show-sidebar").click(function () {
+    $(".page-wrapper").addClass("toggled");
+  });
+});
+
+$(window).resize(function (e) {
+  if ($(window).width() <= 768) {
+    $(".page-wrapper").removeClass("toggled");
+  } else {
+    $(".page-wrapper").addClass("toggled");
+  }
+});
