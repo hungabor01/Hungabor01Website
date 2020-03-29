d3084991971b75e56102dc7798162e182dd179b3
hungabor01 <hungabor01@gmail.com>
2020-03-29 16:40:34 +0200

    22-MenuAndBasicPages

---
+++ Hungabor01Website/Hungabor01Website/Controllers/ArticlesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hungabor01Website.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
